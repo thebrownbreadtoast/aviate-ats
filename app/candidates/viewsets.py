6d5bb524df5df49f536fa648e069bf07811bfb1e
thebrownbreadtoast <akshayd00@outlook.com>
2025-03-13 00:42:08 +0530

    feat: /search API decoupled from default .list method

--- app/candidates/viewsets.py
+++ app/candidates/viewsets.py
@@ -1,5 +1,7 @@
 from django.db.models import Case, IntegerField, Value, When
+from rest_framework.decorators import action
 
+from app.base import responses
 from app.base.viewsets import BaseModelViewSet
 from app.candidates.models import Candidate
 from app.candidates.serializers import CandidateSerializer
@@ -9,32 +11,47 @@ class CandidateViewSet(BaseModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
+    def get_relevant_candidates(self, query):
+        queryset = self.get_queryset()
 
-        search_query = self.request.query_params.get("search", "")
+        words = [word.strip() for word in query.split() if word.strip()]
 
-        if search_query:
-            words = [word.strip() for word in search_query.split() if word.strip()]
+        if not words:
+            return Candidate.objects.none()
 
-            if not words:
-                return Candidate.objects.none()
+        relevancy_expr = Value(0, output_field=IntegerField())
 
-            relevancy_expr = Value(0, output_field=IntegerField())
-
-            for word in words:
-                case_expr = Case(
-                    When(name__icontains=word, then=Value(1)),
-                    default=Value(0),
-                    output_field=IntegerField(),
-                )
-
-                relevancy_expr += case_expr
-
-            queryset = (
-                queryset.annotate(relevancy=relevancy_expr)
-                .filter(relevancy__gt=0)
-                .order_by("-relevancy", "name")
+        for word in words:
+            case_expr = Case(
+                When(name__icontains=word, then=Value(1)),
+                default=Value(0),
+                output_field=IntegerField(),
             )
 
+            relevancy_expr += case_expr
+
+        queryset = (
+            queryset.annotate(relevancy=relevancy_expr)
+            .filter(relevancy__gt=0)
+            .order_by("-relevancy", "name")
+        )
+
         return queryset
+
+    def list(self, request):
+        return responses.NotImplemented(
+            {"error": "List API not implemented for this resource."}
+        )
+
+    @action(detail=False, methods=["GET"])
+    def search(self, request):
+        query = request.query_params.get("q", "")
+
+        if not query:
+            return responses.BadRequest({"error": "Please enter valid search query."})
+
+        relevant_candidates_qs = self.get_relevant_candidates(query)
+
+        serializer = self.get_serializer(relevant_candidates_qs, many=True)
+
+        return responses.Ok(serializer.data)
